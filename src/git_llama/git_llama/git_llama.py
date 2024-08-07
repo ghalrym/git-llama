8813a5be075a8265dbb46ef14febbf75e57f080f
ghalrym <andrewreed2015@gmail.com>
2024-08-07 00:24:14 -0700

    feat: generate git commit message based on diffs and conventional format
    BREAKING CHANGES:
    
    This commit will take the current git diffs, parse them into a human-readable format, and then use the LLAMA model to generate a commit message in conventional format.

---
+++ src/git_llama/git_llama/git_llama.py
@@ -0,0 +1,41 @@
+import os
+import pathlib
+
+from langchain_community.chat_models import ChatOllama
+from langchain_core.messages import SystemMessage, HumanMessage
+
+from git import Git
+from git_llama.prompts import COMMIT_PROMPT, FORMAT_EXPLAINED
+
+
+FORMAT_DIR = os.path.join(pathlib.Path(__file__).parent.resolve(), "formats/")
+
+
+class GitLlama:
+
+    @staticmethod
+    def write_git_commit(files: list[str] | None = None) -> str:
+        llama_chat = ChatOllama(model="llama3.1")
+        messages = [
+            SystemMessage(
+                os.getenv('GIT_LLAMA_COMMIT_PROMPT', COMMIT_PROMPT) +
+                "\n\n\n" +
+                os.getenv(
+                    'GIT_LLAMA_FORMAT_EXPLAINED',
+                    FORMAT_EXPLAINED.format(
+                        format_explained=open(rf"{FORMAT_DIR}\conventional_format.md").read()
+                    )
+                )
+            ),
+            HumanMessage("\n".join(
+                str(diff)
+                for diff in Git.diffs()
+                if files is None or diff.new_file_name in files
+            ))
+        ]
+        for message in messages:
+            print(message.content)
+            print("\n\n\n")
+        response = llama_chat.invoke(messages)
+        return response.content
+
