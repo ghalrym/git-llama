7cf5f8f7708460958a405c0fd0b947c0332d8309
ghalrym <andrewreed2015@gmail.com>
2024-08-06 23:49:07 -0700

    Added Git class to encapsulate git commands, improved formatting and structure.

--- src/git_llama/git/git.py
+++ src/git_llama/git/git.py
@@ -1,4 +1,5 @@
 import re
+import subprocess
 
 CLEAN_GIT_BRANCH = re.compile("(\s*\*\s*)|(\s*remotes/origin/\s*)")
 
@@ -14,3 +15,68 @@ GIT_DIFF_START_FORMAT = re.compile(
     "(?:-{3}|\+{3}) b/(.*)\n"
     "[^\n]*\n"
 )
+
+
+class Git:
+
+    @staticmethod
+    def fetch():
+        subprocess.check_output(["git", "fetch"]).decode()
+
+    @staticmethod
+    def log():
+        """Returns the git log as a list of GitLog objects"""
+        log = subprocess.check_output(["git", "log"]).decode()
+        return [
+            GitLog(
+                commit_hash=group[0].strip(),
+                author=group[1].strip(),
+                author_email=group[2].strip(),
+                date=group[3].strip(),
+                message=group[4].strip(),
+            )
+            for group in GIT_LOG_FORMAT.findall(log)
+        ]
+
+    @staticmethod
+    def diffs():
+        """Returns the current diff"""
+        remainder = subprocess.check_output(["git", "diff"]).decode()
+        groups = GIT_DIFF_START_FORMAT.findall(remainder)
+        diffs_formatted: str = GIT_DIFF_START_FORMAT.sub("=========NEXT" + "_DIFF============\n", remainder)
+        diffs = diffs_formatted.split("=========NEXT" + "_DIFF============\n")[1:]
+
+        return [
+            GitDiff(
+                current_file_name=group[0].strip(),
+                new_file_name=group[1].strip(),
+                diff=diff_text
+            )
+            for group, diff_text in zip(groups, diffs)
+        ]
+
+    @staticmethod
+    def branches():
+        log = subprocess.check_output(["git", "branch", "-a"]).decode()
+        return [
+            GitBranch(
+                name=CLEAN_GIT_BRANCH.sub("", branch).strip(),
+                is_local=is_local
+            )
+            for branch in log.splitlines()
+            if "->" not in branch
+            if (is_local := "remotes/" not in branch) or True
+        ]
+
+    @staticmethod
+    def local_branches():
+        return [branch for branch in Git.branches() if branch.is_local]
+
+    @staticmethod
+    def remove_branches():
+        return [branch for branch in Git.branches() if not branch.is_local]
+
+    @staticmethod
+    def origin_head_branch():
+        log = subprocess.check_output(["git", "branch", "-r"]).decode()
+        return GIT_ORIGIN_BRANCH_FORMAT.findall(log)[0]
