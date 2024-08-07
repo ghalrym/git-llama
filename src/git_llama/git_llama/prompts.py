f75cba084a6af549073aa0dd20ac447c34aa7105
ghalrym <andrewreed2015@gmail.com>
2024-08-07 00:16:01 -0700

    feat: Add prompts

---
+++ src/git_llama/git_llama/prompts.py
@@ -0,0 +1,12 @@
+
+COMMIT_PROMPT = """\
+Write a git commit message about the changes in the git diffs.
+Use conventional commit format.
+Do not include ticket numbers in the response.
+ONLY RESPOND WITH THE COMMIT MESSAGE!
+"""
+
+FORMAT_EXPLAINED = """
+Please follow the format in the following markdown file:
+{format_explained}
+"""
